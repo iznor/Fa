8eb921b92cae4bcec71de2dd838b13a6ad580f8e
iZnoR <76132625+iznor@users.noreply.github.com>
2021-12-16 17:30:49 +0200

    jquery-hide-visible-before-fix

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,36 +1,11 @@
-let colorBank =
-    [
-        '#e2b699b7',
-        '#e2ce99b7',
-        '#d2e299b7',
-        '#aee299b7',
-        '#99e2d0b7',
-        '#99bfe2b7',
-        '#9e99e2b7',
-        '#af99e2b7',
-        '#e299dcb7',
-        '#e29999b7'
-    ];
 
-let backgroundBank = 
-    [
-        'url("../assets/img/random-backgrounds/Woman_singer_icon.svg")',
-        'url("../assets/img/random-backgrounds/arnold-schwarzenegger-stencil.svg")',
-        'url("../assets/img/random-backgrounds/jimi-hendrix-stencil.svg")',
-        'url("../assets/img/random-backgrounds/kanye-west-stencil.svg")',
-        'url("../assets/img/random-backgrounds/leonardo-dicaprio-stencil.svg")',
-        'url("../assets/img/random-backgrounds/monalisa-stencil.svg")',
-        'url("../assets/img/random-backgrounds/musician-vector-free.svg")',
-        'url("../assets/img/random-backgrounds/nelson-mandela-stencil.svg")',
-        'url("../assets/img/random-backgrounds/peter-pan-stencil.svg")',
-        'url("../assets/img/random-backgrounds/thanksgiving-owl-stencil.svg")'
-    ];
-
+//todo: check last id (from obj array)
 let widgetId = 0;
 
 $(".widget.append").click(() => {
+    const thisWidgetId = widgetId;
     $(".dashboard").append(`\
-        <div id="w-${widgetId}" class="widget">
+        <div id="w-${thisWidgetId}" class="widget visible">
             <div class="background"></div>
             <h1>Top 5 Songs By Eminem:</h1>
             <div class="content">CONTENT FROM DATABASE</div>
@@ -38,29 +13,58 @@ $(".widget.append").click(() => {
                 <div class="button-container delete-button"></div>
                 <div class="button-container edit-button"></div>
             </div>
+        </div>
+        <div class="widget edit hide">
+        <h1>Customize:</h1>
+        <div class="content">
+            <div class="row">
+                <div class="category">Top</div>
+                <div class="category choice">CHOOSE</div>
+            </div>
+            <div class="row">
+                <div class="category">By</div>
+                <div class="category choice">CHOOSE</div>
+            </div>
+            <div class="row">
+                <div class="category">Choose</div>
+                <div class="category choice">ARTIST/GENRE</div>
+            </div>
         </div>`);
     let randomColor = colorBank[Math.trunc(Math.random() * colorBank.length)];
     let randomBackground = backgroundBank[Math.trunc(Math.random() * backgroundBank.length)];
     $(`#w-${widgetId}`)
-        .css("background-color",`${randomColor}`)
+        .css("background-color", `${randomColor}`)
     $(`#w-${widgetId} .background`)
-        .css("background-image",`${randomBackground}`)   
+        .css("background-image", `${randomBackground}`)
+
+    $('.button-container.edit-button')
+        .off()
+        .on('click', (element) => {
+            const widget=element.target.parentElement.parentElement.parentElement;
+            const hide=widget.querySelector('.hide')
+            const visible=widget.querySelector('.visible')
+            console.log(hide,visible);
+            visible.classList.replace('visible','hide');
+            hide.classList.replace('hide','visible');
+        })
+    $('.button-container.delete-button')
+        .off()
+        .click((element) => {
+            const widget = element.target.parentElement.parentElement;
+            widget.parentElement.removeChild(widget)
+
+        })
+    $('.button-container.back-button')
+        .off()
+        .click(() => {
+            alert("Back")
+        })
+
+    $('.button-container.done-button')
+        .off()
+        .click(() => {
+            alert("Done")
+        })
     ++widgetId;
 });
 
-$('.button-container .edit-button')
-    .click(()=>{
-        alert("Edit")
-    })
-$('.button-container.delete-button')
-    .click(()=>{
-        alert("Delete")
-    })
-$('.button-container.back-button')
-    .click(()=>{
-        alert("Back")
-    })
-$('.button-container.done-button')
-    .click(()=>{
-        alert("Done")
-    })
